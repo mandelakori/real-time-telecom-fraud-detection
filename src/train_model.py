cf7d7be7f1199d9d1905a073b3cc621054f35f3c
Mandela Logan <mandelakorilogan@gmail.com>
2025-08-26 04:34:48 -0400

    added dependencies for training

---
+++ src/train_model.py
@@ -0,0 +1,7 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import classification_report
+import joblib
+from preprocessing import clean_text
